da4ccd543b8dbd49276ddea386528f54f5b2811a
David Maciel <david.maciel94@gmail.com>
2020-12-02 13:48:01 -0300

    feito tela de edit

--- app/Http/Controllers/Form/TestController.php
+++ app/Http/Controllers/Form/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Form;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Hash;
 
 
 class TestController extends Controller
@@ -30,8 +31,12 @@ class TestController extends Controller
     public function storeUser(Request $request){
         $user = new User();
         $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
+        if(filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+            $user->email = $request->email;
+        }else{
+            echo("$request->email não é valido");
+        }
+        $user->password = hash::make($request->password);
         $user->save(); //preciso desse save para persistir as informaçoes no banco
         return redirect()->route('users.listAll');  //retorna na pagina de home
         //dd($request);
@@ -43,8 +48,18 @@ class TestController extends Controller
         ]);                                     //através do value=""
     }
 
-    public function edit(User $user, Request $request){
-        var_dump($user, $request);
+    public function edit(User $user, Request $request){ //vem os dados do formulario editUser
+        $user->name = $request->name;
+
+        if(filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+              $user->email = $request->email;
+         }                                         //recebo os dados via request e passo e pra 
+                                                    // dentro do user pra persistir no banco
+        if(!empty($request->password)){
+            $user->password = hash::make($request->password);
+        }
+        $user->save();
+        return redirect()->route('users.listAll');     //redireciona para a tela de usuarios
     }
 
     
