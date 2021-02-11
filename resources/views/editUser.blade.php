88b26642d681f74ecdddbd65764156ce2da20f8d
David Maciel <david.maciel94@gmail.com>
2021-02-11 19:49:15 -0300

    fsfa

--- resources/views/editUser.blade.php
+++ resources/views/editUser.blade.php
@@ -23,7 +23,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Editar Usuarios</title>
+    <title>Editar Usuarioss</title>
 </head>
 <body>
                     
@@ -37,7 +37,7 @@
         </div>
 
         <div class="form-group">
-          <label for="">E-mail do Usuário: </label>
+          <label for="">E-mail do Usuários: </label>
           <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={{$user->email}}>
         </div>
 
